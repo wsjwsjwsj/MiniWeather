40efe7b59ee574cc500c73cece1aa0963a220ea8
wsjwsjwsj <632069405@qq.com>
2016-12-24 15:08:41 +0800

    借助友盟实现消息推送功能。没有在友盟上找到自动更新和用户反馈功能。

--- app/src/main/java/com/example/administrator/miniweather/MyApplication.java
+++ app/src/main/java/com/example/administrator/miniweather/MyApplication.java
@@ -4,6 +4,9 @@ import android.app.Application;
 import android.os.Environment;
 import android.util.Log;
 
+import com.umeng.message.IUmengRegisterCallback;
+import com.umeng.message.PushAgent;
+
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
@@ -24,6 +27,22 @@ public class MyApplication extends Application{
     @Override
     public void onCreate() {
         super.onCreate();
+        PushAgent mPushAgent = PushAgent.getInstance(this);
+        //注册推送服务，每次调用register方法都会回调该接口
+        mPushAgent.register(new IUmengRegisterCallback() {
+
+            @Override
+            public void onSuccess(String deviceToken) {
+                Log.d("deviceToken",deviceToken);
+                //注册成功会返回device token
+            }
+
+            @Override
+            public void onFailure(String s, String s1) {
+                Log.d("deviceToken","fail");
+            }
+        });
+
         myApplication=this;
         cityDB=openCityDB();
         initCityList();
